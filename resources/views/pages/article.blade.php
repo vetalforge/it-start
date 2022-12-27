b673eacf57f0a9f32055aa112764b157b5ccf1fd
Vitaliy Svyatodukh <v.sviatodukh@m2e.team>
2022-12-27 20:50:49 +0200

    articles #2

--- resources/views/pages/article.blade.php
+++ resources/views/pages/article.blade.php
@@ -1,6 +1,16 @@
 @extends('layouts.main')
 
 @section('content')
-    {{ $title}}
-    {{ $description}}
+    <div class="container-fluid">
+        <div class="container py-5">
+            <div class="row mx-0 justify-content-center">
+                <div class="section-title text-center position-relative mb-5">
+                    <h1 class="display-4">{{ $title}}</h1>
+                </div>
+                <p>
+                    {{ $description}}
+                </p>
+            </div>
+        </div>
+    </div>
 @endsection
