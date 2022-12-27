f1cede11527a1f9c2e4a6b12110b5a84bf3674f0
Vitaliy Svyatodukh <v.sviatodukh@m2e.team>
2022-12-27 19:45:53 +0200

    calendar icon at main page

--- resources/views/pages/course_card_main.blade.php
+++ resources/views/pages/course_card_main.blade.php
@@ -5,7 +5,7 @@
         <div class="border-top w-100 mt-3">
             <div class="d-flex justify-content-between p-4">
                 <span class="text-white"><i class="fa fa-user mr-2"></i>{{ $course->age }}</span>
-                <span class="text-white"><i class="fa fa-star mr-2"></i>{{ $course->duration }}</span>
+                <span class="text-white"><i class="fa fa-calendar mr-2"></i>{{ $course->duration }}</span>
             </div>
         </div>
         <div class="w-100 bg-white text-center p-4" >
