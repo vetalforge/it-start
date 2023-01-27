3e275a75d3fff127c75abacee3e88fb3b26f426a
Vitaliy Svyatodukh <heavyvetal15@gmail.com>
2023-01-27 21:17:50 +0200

    404 for wrong article urls

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -9,16 +9,20 @@ class ArticlesController extends Controller
 {
     public function index(Request $request)
     {
-        $article = Article::where('name', $request->name)->first();
-        $language = app()->getLocale();
-        $title = 'title_' . $language;
-        $description = 'description_' . $language;
-        $title = $article->$title;
-        $description = $article->$description;
+        try {
+            $article = Article::where('name', $request->name)->first();
+            $language = app()->getLocale();
+            $title = 'title_' . $language;
+            $description = 'description_' . $language;
+            $title = $article->$title;
+            $description = $article->$description;
 
-        return view('pages.article', [
-            'title' => $title,
-            'description' => $description,
-        ]);
+            return view('pages.article', [
+                'title' => $title,
+                'description' => $description,
+            ]);
+        } catch (\Exception $e) {
+            return response()->view('errors.404', [], 404);
+        }
     }
 }
