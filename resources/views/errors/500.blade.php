e687f2d04edd984bfa7a22d01a652a9388baa643
Vitaliy Svyatodukh <v.sviatodukh@m2e.team>
2023-03-06 18:22:51 +0200

    errors 500

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="container-fluid">
+        <div class="container py-5">
+            <div class="row mx-0 justify-content-center">
+                <div class="content-wrapper align-content-center">
+                    <h1 style="font-size: 92px; text-align: center">500</h1>
+                    <h3>Oops! Something is wrong</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
