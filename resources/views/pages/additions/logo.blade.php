61e7d6e1cb4666aa7d7cf12bc9fc1a5469f0fbd4
Vitaliy Svyatodukh <v.sviatodukh@m2e.team>
2023-05-25 17:25:58 +0300

    new name and logo #2

---
+++ resources/views/pages/additions/logo.blade.php
@@ -0,0 +1,10 @@
+<div class="row" style="align-items: center;">
+    <div class="wrapper" style="display: flex; width: 45px; margin: 0 8px; margin-bottom: 2px;">
+        <img src="/img/new_logo.png" alt="logo" style="width: 100%;">
+    </div>
+    <div class="text">
+        <h1 class="m-0 text-primary" style="font-size: 3rem;">
+            @lang('main.school_name')<span style="color:#fedc00;text-shadow: 1px 0 0 #f14d5d;">IT</span>
+        </h1>
+    </div>
+</div>
