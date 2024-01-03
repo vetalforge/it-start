fed6a4a6496f494338bcfae074bb4a998a7237c2
Vitaliy Svyatodukh <v.sviatodukh@m2e.team>
2024-01-03 12:21:32 +0200

    migration: url to urls

--- database/migrations/2024_01_02_170213_urls_table.php
+++ database/migrations/2024_01_02_170213_urls_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('url', function (Blueprint $table) {
+        Schema::create('urls', function (Blueprint $table) {
             $table->id();
             $table->string('name');
         });
